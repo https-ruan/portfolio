1af81cc9b27960318fc60e80a8e3aff65797a4de
Ruan Araújo <ruands2002@gmail.com>
2023-05-11 15:05:20 -0300

    feat(app): created dynamic title, header shadow and added more default styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { HeaderComponent } from '@shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-header />
+    <app-header container />
     <router-outlet></router-outlet>
     <app-footer />
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  @ViewChild(HeaderComponent, { read: ElementRef })
+  private readonly header!: ElementRef;
+
+  constructor(
+    private readonly _titleService: Title,
+    private readonly r2: Renderer2
+  ) {
+    this.dynamicTitle();
+  }
+
+  private dynamicTitle() {
+    const originalTitle = this._titleService.getTitle();
+    const steps = '/-\\|';
+    const FPS = 2;
+    const frameInterval = 1000 / FPS;
+
+    let step = 0;
+    let lastTimestep = 0;
+
+    const animation = (timestamp: number) => {
+      if (lastTimestep + frameInterval < timestamp) {
+        this._titleService.setTitle(`${steps[step++]} ${originalTitle}`);
+
+        step %= steps.length;
+        lastTimestep = timestamp;
+      }
+
+      window.requestAnimationFrame(animation);
+    };
+
+    window.requestAnimationFrame(animation);
+  }
+
+  @HostListener('window:scroll', ['$event']) private onScroll(e: Event): void {
+    const window = (e.target as Document).defaultView as Window;
+    const header = this.header.nativeElement;
+
+    if (!window.scrollY) {
+      this.r2.removeClass(header, 'shadow');
+    } else {
+      this.r2.addClass(header, 'shadow');
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FooterComponent } from './@components/footer/footer.component';
-import { HeaderComponent } from './@components/header/header.component';
+import { SharedModule } from '@shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-const mainComponents = [HeaderComponent, FooterComponent];
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, ...mainComponents, AppRoutingModule],
+  imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
