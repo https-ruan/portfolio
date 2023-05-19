6c741e77721c62344340ce7f9b33d284e84078e9
Ruan Araújo <ruands2002@gmail.com>
2023-05-19 10:50:03 -0300

    feat(about): added component

--- src/app/sections/sections.module.ts
+++ src/app/sections/sections.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { NgxTypedJsModule } from 'ngx-typed-js';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+
+const sections = [WelcomeComponent, AboutComponent];
 
 @NgModule({
-  declarations: [WelcomeComponent],
+  declarations: [...sections],
   imports: [CommonModule, SharedModule, NgxTypedJsModule],
-  exports: [WelcomeComponent],
+  exports: [...sections],
 })
 export class SectionsModule {}
