f02c98aad62829417fe23df65777a6dacb93e42d
Ruan Araújo <ruands2002@gmail.com>
2023-05-26 19:22:54 -0300

    feat(footer): translation included

--- src/app/@shared/components/footer/footer.component.ts
+++ src/app/@shared/components/footer/footer.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { Constants } from '@core/constants';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  public myself = Constants.myself;
+
   public get currentYear(): number {
     return new Date().getFullYear();
   }
