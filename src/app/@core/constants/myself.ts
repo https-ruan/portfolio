249659c9cc5dd0b8aae061bf9e60faa547ec6cae
Ruan Araújo <ruands2002@gmail.com>
2023-05-26 14:17:20 -0300

    chore(myself): updated myself const variable

--- src/app/@core/constants/myself.ts
+++ src/app/@core/constants/myself.ts
@@ -1,9 +1,15 @@
 import { age } from '@shared/functions';
 
 export const myself = Object.freeze({
-  name: 'Ruan Araújo',
-  age: age('08-20-2002'),
-  role: 'Front-end Developer',
-  currentJob: 'IBM',
-  description: 'Welcome to my portfolio! 🙂',
+  resume: {
+    name: 'Ruan Araújo',
+    age: age('08-20-2002'),
+    role: 'Front-end Developer',
+    currentJob: 'IBM',
+    description: 'Welcome to my portfolio! 🙂',
+  },
+  yearsExperience: 3,
+  coursesAndCertifications: 10,
+  workedProjects: 12,
+  hobbies: ['Musics', 'Films and Series', 'Games'],
 });
