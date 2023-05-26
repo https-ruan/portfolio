7369fb2fe35c9ad131fafa3168a767c48026bfa6
Ruan Araújo <ruands2002@gmail.com>
2023-05-26 18:31:52 -0300

    feat(header): partial translations included

--- src/app/@shared/components/header/header.component.ts
+++ src/app/@shared/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { NgxTypedJsModule } from 'ngx-typed-js';
 import { routes } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, NgxTypedJsModule],
+  imports: [CommonModule, RouterModule, NgxTypedJsModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
