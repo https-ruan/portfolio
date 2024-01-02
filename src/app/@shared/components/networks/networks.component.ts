e5390b9dd70759af7eb52f32f690c369761d96f8
Ruan Araújo <ruands2002@gmail.com>
2024-01-02 16:42:41 -0300

    feat(networks): finished component

--- src/app/@shared/components/networks/networks.component.ts
+++ src/app/@shared/components/networks/networks.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { Constants } from '@core/constants';
 import { ELanguages } from '@core/i18n';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-networks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule, MatTooltipModule],
   templateUrl: './networks.component.html',
   styleUrls: ['./networks.component.scss'],
 })
@@ -21,10 +22,13 @@ export class NetworksComponent {
   constructor(private readonly _translateService: TranslateService) {}
 
   public changeLng(): void {
-    if (this.currentLng === ELanguages.ptBR) {
-      this._translateService.use(ELanguages.enUS);
-    } else {
-      this._translateService.use(ELanguages.ptBR);
+    switch (this.currentLng) {
+      case ELanguages.ptBR:
+        this._translateService.use(ELanguages.enUS);
+        break;
+      default:
+        this._translateService.use(ELanguages.ptBR);
+        break;
     }
   }
 }
